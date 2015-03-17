8715ad25a17c91ddde2f357ec0293fb8aac86870
Carlos Malavé <hellfish2@gmail.com>
2015-03-17 14:13:03 -0430

    First Push

---
+++ API/api2/include/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "android_api");
+define("DB_PASSWORD", "123456");
+define("DB_DATABASE", "android_api");
+?>
