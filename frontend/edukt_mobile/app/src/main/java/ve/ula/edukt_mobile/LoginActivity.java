fcf34f81cc8bbd4f535d4ddb6c48f8eb74c0fad1
Juan Gomez <jgomezbjj@gmail.com>
2015-06-01 17:32:39 -0430

    frontend edukt_mobile actualizado

---
+++ frontend/edukt_mobile/app/src/main/java/ve/ula/edukt_mobile/LoginActivity.java
@@ -0,0 +1,155 @@
+
+package ve.ula.edukt_mobile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import ve.ula.edukt_mobile.library.DatabaseHandler;
+import ve.ula.edukt_mobile.library.UserFunctions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginActivity extends Activity {
+	Button btnLogin;
+	Button btnLinkToRegister;
+	EditText inputEmail;
+	EditText inputPassword;
+	TextView loginErrorMsg;
+
+
+	// JSON Response node names
+	private static String KEY_SUCCESS = "success";
+	private static String KEY_ERROR = "error";
+	private static String KEY_ERROR_MSG = "error_msg";
+	private static String KEY_UID = "uid";
+	private static String KEY_NAME = "name";
+	private static String KEY_EMAIL = "email";
+	private static String KEY_CREATED_AT = "created_at";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+
+        //Add to prevent the emulator localhost main threat error
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        //Borrar solo es para saltar el login
+        //Intent dashboard = new Intent(getApplicationContext(), MainActivity.class);
+        //startActivity(dashboard);
+
+		// Importing all assets like buttons, text fields
+		inputEmail = (EditText) findViewById(R.id.loginEmail);
+		inputPassword = (EditText) findViewById(R.id.loginPassword);
+		btnLogin = (Button) findViewById(R.id.btnLogin);
+		btnLinkToRegister = (Button) findViewById(R.id.btnLinkToRegisterScreen);
+		loginErrorMsg = (TextView) findViewById(R.id.login_error);
+
+		// Login button Click Event
+		btnLogin.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View view) {
+				String email = inputEmail.getText().toString();
+				String password = inputPassword.getText().toString();
+				UserFunctions userFunction = new UserFunctions();
+				Log.d("Button", "Login");
+
+                //Validate if there is communication to api2 server
+                try {
+                    JSONObject json = userFunction.loginUser(email, password);
+                    // check for login response
+                    try {
+                        if (json.getString(KEY_SUCCESS) != "success") {
+                            loginErrorMsg.setText("");
+                            String res = json.getString(KEY_SUCCESS);
+                            if(Integer.parseInt(res) == 1){
+                                // user successfully logged in
+                                // Store user details in SQLite Database
+                                DatabaseHandler db = new DatabaseHandler(getApplicationContext());
+                                JSONObject json_user = json.getJSONObject("user");
+
+                                // Clear all previous data in database
+                                userFunction.logoutUser(getApplicationContext());
+                                db.addUser(json_user.getString(KEY_NAME), json_user.getString(KEY_EMAIL), json.getString(KEY_UID), json_user.getString(KEY_CREATED_AT));
+
+                                // Launch Dashboard Screen
+                                //Intent dashboard = new Intent(getApplicationContext(), DashboardActivity.class);
+                                Intent dashboard = new Intent(getApplicationContext(), MainActivity.class);
+
+                                // Close all views before launching Dashboard
+                                //dashboard.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(dashboard);
+
+                                // Close Login Screen
+                                finish();
+                            }else{
+                                // Error in login
+                                loginErrorMsg.setText("Incorrect username/password");
+                            }
+                        }
+                    } catch (JSONException e) {
+
+                        e.printStackTrace();
+
+                    }
+
+
+                }catch(Exception e){
+                    Log.d("Button", "Loginooooo fail");
+                    loginErrorMsg.setText("API2. server unreachable");
+                }
+
+
+			}
+		});
+
+		// Link to Register Screen
+		btnLinkToRegister.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View view) {
+				Intent i = new Intent(getApplicationContext(),
+						RegisterActivity.class);
+				startActivity(i);
+				finish();
+			}
+		});
+	}
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);}
+
+
+}
---
+++ frontend/edukt_mobile/app/src/main/java/ve/ula/edukt_mobile/TimeLineFragment.java
@@ -0,0 +1,214 @@
+package ve.ula.edukt_mobile;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.MenuInflater;
+
+import ve.ula.edukt_mobile.adapter.FeedListAdapter;
+import ve.ula.edukt_mobile.app.AppController;
+import ve.ula.edukt_mobile.data.FeedItem;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Cache;
+import com.android.volley.Cache.Entry;
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TimeLineFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TimeLineFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private ListView listView;
+    private FeedListAdapter listAdapter;
+    private List<FeedItem> feedItems;
+    private String URL_FEED = "http://api.androidhive.info/feed/feed.json";
+
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment TimeLineFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static TimeLineFragment newInstance(String param1, String param2) {
+        TimeLineFragment fragment = new TimeLineFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public TimeLineFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_time_line, container, false);
+        //return inflater.inflate(R.layout.fragment_time_line, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_time_line, container, false);
+        listView = (ListView) view.findViewById(R.id.list);
+
+
+
+        feedItems = new ArrayList<FeedItem>();
+
+        //listAdapter = new FeedListAdapter(this, feedItems);
+        listAdapter = new FeedListAdapter(getActivity(), feedItems);
+        listView.setAdapter(listAdapter);
+
+        // We first check for cached request
+        Cache cache = AppController.getInstance().getRequestQueue().getCache();
+        Entry entry = cache.get(URL_FEED);
+        if (entry != null) {
+            // fetch the data from cache
+            try {
+                String data = new String(entry.data, "UTF-8");
+                try {
+                    parseJsonFeed(new JSONObject(data));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            // making fresh volley request and getting json
+            JsonObjectRequest jsonReq = new JsonObjectRequest(Method.GET,
+                    URL_FEED, null, new Response.Listener<JSONObject>() {
+
+                @Override
+                public void onResponse(JSONObject response) {
+                    VolleyLog.d(TAG, "Response: " + response.toString());
+                    if (response != null) {
+                        parseJsonFeed(response);
+
+                    }
+                }
+            }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    VolleyLog.d(TAG, "Error: " + error.getMessage());
+                }
+            });
+
+            // Adding request to volley request queue
+            AppController.getInstance().addToRequestQueue(jsonReq);
+        }
+
+        return view;
+
+
+    }
+
+
+    /**
+     * Parsing json reponse and passing the data to feed view list adapter
+     * */
+    private void parseJsonFeed(JSONObject response) {
+        try {
+            JSONArray feedArray = response.getJSONArray("feed");
+
+            for (int i = 0; i < feedArray.length(); i++) {
+                JSONObject feedObj = (JSONObject) feedArray.get(i);
+
+                FeedItem item = new FeedItem();
+                item.setId(feedObj.getInt("id"));
+                item.setName(feedObj.getString("name"));
+
+                // Image might be null sometimes
+                String image = feedObj.isNull("image") ? null : feedObj
+                        .getString("image");
+                item.setImge(image);
+                item.setStatus(feedObj.getString("status"));
+                item.setProfilePic(feedObj.getString("profilePic"));
+                item.setTimeStamp(feedObj.getString("timeStamp"));
+
+                // url might be null sometimes
+                String feedUrl = feedObj.isNull("url") ? null : feedObj
+                        .getString("url");
+                item.setUrl(feedUrl);
+
+                feedItems.add(item);
+            }
+
+            // notify data changes to list adapater
+            listAdapter.notifyDataSetChanged();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+}
