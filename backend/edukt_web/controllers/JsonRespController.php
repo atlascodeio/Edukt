1526136585946d7fd1d83ea3375b3b1bbc23fccc
Juan Gomez <jgomezbjj@gmail.com>
2015-06-03 19:40:55 -0430

    nuevos metodos json

--- backend/edukt_web/controllers/JsonRespController.php
+++ backend/edukt_web/controllers/JsonRespController.php
@@ -68,7 +68,7 @@ class JsonRespController extends Controller
     		$result[$key_aux]['image'] = null;
     		$result[$key_aux]['status'] = $nota['descripcion'];
     		$result[$key_aux]['profilePic'] = $nota->users->profile_pic;
-    		$result[$key_aux]['timeStamp'] = strtotime($doc['created_at']).'999';
+    		$result[$key_aux]['timeStamp'] = strtotime($nota['created_at']).'999';
     		//$final[$key_aux]['created_at'] = strtotime($doc['created_at']);
     		$result[$key_aux]['url'] = null;
     	}
@@ -105,6 +105,90 @@ class JsonRespController extends Controller
     	$json = json_encode($final);
     	echo $json;
     	
+    }
+    
+    /**
+     * Return JsonArray with JsonObjects containing list of notas
+     * @return mixed
+     */
+    public function actionGetNotifications()
+    {
+    
+    	//find all the docs in array
+    	$notas = Notas::find()->orderBy('uid DESC')->all();
+    	 
+    	//prepare final array with results
+    	$result = array();
+    	foreach($notas as $key=>$nota){
+    		$result[$key]['id'] = $nota['uid'];
+    		$result[$key]['name'] = $nota->users->name;
+    		$result[$key]['image'] = null;
+    		$result[$key]['status'] = $nota['descripcion'];
+    		$result[$key]['profilePic'] = $nota->users->profile_pic;
+    		$result[$key]['timeStamp'] = strtotime($nota['created_at']).'999';
+    		$result[$key]['url'] = null;
+    	}
+    	 
+    	$final = array('feed'=>$result);
+    	$json = json_encode($final);
+    	echo $json;
+    	 
+    }
+    
+    
+    /**
+     * Return JsonArray with JsonObjects containing list of docs
+     * @return mixed
+     */
+    public function actionGetDocuments()
+    {
+    
+    	//find all the docs in array
+    	$docs = Docs::find()->orderBy('uid DESC')->with('users')->all();
+    
+    	//prepare final array with results
+    	$result = array();
+    	foreach($docs as $key=>$doc){
+    		$result[$key]['id'] = $doc->uid;
+    		$result[$key]['name'] = $doc->users->name;
+    		$result[$key]['image'] = null;
+    		$result[$key]['status'] = $doc->descripcion;
+    		$result[$key]['profilePic'] = $doc->users->profile_pic;
+    		$result[$key]['timeStamp'] = strtotime($doc->created_at).'999';
+    		$result[$key]['url'] = $doc->url_doc;
+    	}
+    
+    	$final = array('feed'=>$result);
+    	$json = json_encode($final);
+    	echo $json;
+    
+    }
+    
+    /**
+     * Return JsonArray with JsonObjects containing data of user loged account
+     * @return mixed
+     */
+    public function actionGetMyAccount()
+    {
+    
+    	$id = 10;
+    	//find the user loged data object
+    	$user = Users::findOne(['uid'=>$id]);
+    
+    	//prepare final array with results
+    	$result = array();
+    	$result['id'] = $user->uid;
+    	$result['name'] = $user->name;
+    	$result['image'] = null;
+    	$result['status'] = 'Universidad: '.$user->universidad->nombre;
+    	$result['profilePic'] = $user->profile_pic;
+    	$result['timeStamp'] = null;
+    	$result['url'] = null;
+    	$result['email'] = 'Email: '.$user->email;
+    	
+    	$json = json_encode($result);
+    	echo $json;
+    
     }
     
     
