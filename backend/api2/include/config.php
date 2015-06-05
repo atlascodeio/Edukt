4c32241a44389a7d15a8c1e426e1ad341d54309b
Juan Gomez <jgomezbjj@gmail.com>
2015-06-04 21:54:49 -0430

    api2 modificado

---
+++ backend/api2/include/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "i3soluciones.com");
+define("DB_USER", "android_api");
+define("DB_PASSWORD", "Inicio01");
+define("DB_DATABASE", "android_api");
+?>
